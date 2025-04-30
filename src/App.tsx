b346bd8a0ca2446218e8e5860c8799cb678955b1
Pmedina30 <pedromedina1508@icloud.com>
2025-04-30 02:08:22 -0400

    commit de prueba

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import ResidentLogin from './components/ResidentLogin';
 import AdminLogin from './components/AdminLogin';
-
+/** */
 const App: React.FC = () => {
   return (
     <Routes>
