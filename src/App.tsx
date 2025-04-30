7301661c2abc82391d1498c02ba9fa58f2c1f62b
Pmedina30 <pedromedina1508@icloud.com>
2025-04-30 01:32:39 -0400

    Codigo-Actualizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import ResidentLogin from './components/ResidentLogin';
+import AdminLogin from './components/AdminLogin';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login-residente" element={<ResidentLogin />} />
+      <Route path="/login-admin" element={<AdminLogin />} />
+      <Route path="/home" element={<Home />} />
+    </Routes>
   );
-}
+};
 
 export default App;
